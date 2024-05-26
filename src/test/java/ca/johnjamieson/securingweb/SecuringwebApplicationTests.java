ff25413b794efee23b2051134d2095b691db1c23
John Jamieson <CrusaderJohn@JohnJamieson.ca>
2024-05-26 14:05:07 -0400

    Project setup with Spring Initializer

---
+++ src/test/java/ca/johnjamieson/securingweb/SecuringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
